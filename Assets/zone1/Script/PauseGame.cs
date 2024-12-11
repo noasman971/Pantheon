1d3982bc86d13f6ec6626a017014fa1d9bfcacb1
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2024-12-11 20:19:50 +0100

    add button to mainmenu

---
+++ Assets/zone1/Script/PauseGame.cs
@@ -0,0 +1,68 @@
+
+using Cainos.PixelArtTopDown_Basic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseGame:MonoBehaviour {
+    private bool pause = false;
+    
+    void Start()
+    { 
+        GameObject.Find("pauseMenu").GetComponent<Canvas>().enabled = false;
+
+    }
+    
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+        
+    
+    }
+
+    public void Resume()
+    {
+        pause = false;
+        GameObject.Find("PF Player").GetComponent<TopDownCharacterController>().enabled = true;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        GameObject.Find("pauseMenu").GetComponent<Canvas>().enabled = false;
+    }
+
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Badis");
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pause = !pause;
+
+            if (pause)
+            {  GameObject.Find("PF Player").GetComponent<TopDownCharacterController>().enabled = false;
+                GameObject.Find("pauseMenu").GetComponent<Canvas>().enabled = true;
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+
+
+
+
+}
