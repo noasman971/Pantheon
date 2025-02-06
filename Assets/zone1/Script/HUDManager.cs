289eff08782ba1e017c039b1a2bb51c480a9470f
KizaGoat <badis.oubaiche@edu.esiee-it.fr>
2025-02-06 09:29:40 +0100

    PNJ AND DIALOGUE

---
+++ Assets/zone1/Script/HUDManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager instance;
+ 
+    public GameObject DialogueHolder;
+    public TextMeshProUGUI nameDisplay, textDisplay;
+    public GameObject ContinueButton;
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    
+    
+}
